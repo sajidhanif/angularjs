31ff9c4f5d2891ad053e1b659543cc5b15795a1c
sajidhanif <hanifsajid@yahoo.co.uk>
2016-11-14 00:51:45 +0000

    Started Project
    Started Project

---
+++ scripts/cribsController.js
@@ -0,0 +1,13 @@
+angular
+    .module('ngCribs')
+    .controller('cribsController', function($scope, cribsFactory) {
+    
+    $scope.cribs;
+    
+    cribsFactory.getCribs().success(function(data) {
+        $scope.cribs = data;
+    }).error(function(error) {
+        console.log(error);
+    });
+    
+});

adfe82d557aa9db95d8f72e3647eed71cb146c5a
sajidhanif <hanifsajid@yahoo.co.uk>
2016-11-17 18:54:22 +0000

    Project AngularJS
    Project AngularJS

--- scripts/cribsFactory.js
+++ scripts/cribsFactory.js
@@ -1,12 +1,12 @@
 angular
-    .module('ngCribs')
-    .factory('cribsFactory', function($http) {
-    
-        function getCribs() {
-        return $http.get('data/data.json');
-    }
-    
-    return {
-        getCribs: getCribs
-    }
-});
+	.module('ngCribs')
+	.factory('cribsFactory', function($http) {
+
+		function getCribs() {
+			return $http.get('data/data.json');
+		}
+
+		return {
+			getCribs: getCribs
+		}
+	});
